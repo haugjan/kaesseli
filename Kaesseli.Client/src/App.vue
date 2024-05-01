81150f332486308db396a90b3b72aa421ea841cd
Jan Haug <jan.haug@digitecgalaxus.ch>
2024-05-01 20:12:02 +0200

    Fix Authentication

--- Kaesseli.Client/src/App.vue
+++ Kaesseli.Client/src/App.vue
@@ -3,28 +3,50 @@
 </template>
 
 <script lang="ts">
-  import { defineComponent } from 'vue';
+  import { defineComponent, onMounted } from 'vue';
   import { msalConfig } from './authConfig.js';
   import { PublicClientApplication } from "@azure/msal-browser";
 
   const msalInstance = new PublicClientApplication(msalConfig);
-  await msalInstance.initialize();
-
-
-  msalInstance.handleRedirectPromise().then((tokenResponse) => {
-    console.log(tokenResponse);
-
-  }).catch((error) => {
-    // handle error, either in the library or coming back from the server
-  });
-
-  try {
-    msalInstance.loginRedirect({});
-  } catch (err) {
-    // handle error
-  }
 
   export default defineComponent({
-    name: 'App'
+    name: 'App',
+    setup() {
+      onMounted(async () => {
+        await msalInstance.handleRedirectPromise().then((tokenResponse) => {
+          console.log(tokenResponse);
+        }).catch((error) => {
+          console.error(error);
+        });
+
+        const accounts = msalInstance.getAllAccounts();
+        if (accounts.length === 0) {
+          try {
+            await msalInstance.loginRedirect({});
+          } catch (err) {
+            console.error(err);
+          }
+        } else {
+          try {
+            const silentResult = await msalInstance.acquireTokenSilent({
+              account: accounts[0],
+              scopes: ["User.Read"] // Ersetzen Sie dies durch die tatsächlichen Bereiche, die Sie benötigen
+            });
+            console.log(silentResult);
+          } catch (err) {
+            if (err instanceof msal.InteractionRequiredAuthError) {
+              // fallback to interaction when silent call fails
+              try {
+                await msalInstance.loginRedirect({});
+              } catch (err) {
+                console.error(err);
+              }
+            } else {
+              console.error(err);
+            }
+          }
+        }
+      });
+    }
   });
 </script>
