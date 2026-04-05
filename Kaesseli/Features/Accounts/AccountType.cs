194b98f089abdb9755640fd98c5f6194ade65e23
Jan Haug <jan.haug@digitecgalaxus.ch>
2026-04-05 21:33:26 +0200

    Move AccountType extension into AccountType.cs

--- Kaesseli/Features/Accounts/AccountType.cs
+++ Kaesseli/Features/Accounts/AccountType.cs
@@ -1,4 +1,4 @@
-﻿namespace Kaesseli.Features.Accounts;
+namespace Kaesseli.Features.Accounts;
 
 public enum AccountType
 {
@@ -6,4 +6,22 @@ public enum AccountType
     Liability = 2,
     Revenue = 3,
     Expense = 4
+}
+
+public static class AccountTypeExtensions
+{
+    extension(AccountType accountType)
+    {
+        // ReSharper disable StringLiteralTypo
+        public string DisplayName() =>
+            accountType switch
+            {
+                AccountType.Asset => "Aktiv",
+                AccountType.Liability => "Passiv",
+                AccountType.Revenue => "Einkommen",
+                AccountType.Expense => "Ausgaben",
+                _ => throw new ArgumentOutOfRangeException(paramName: nameof(accountType), accountType, message: null)
+            };
+        // ReSharper restore StringLiteralTypo
+    }
 }
